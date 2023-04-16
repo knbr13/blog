03186247a9f3e1074d1a6788bfb0eb3439928902
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-16 23:53:53 +0300

    implemented get user function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
-const { signup, login, searchUsers } = require("../controllers/userController");
+const { signup, login, searchUsers, getUser } = require("../controllers/userController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const router = require("express").Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/search",authMiddleware, searchUsers);
+router.get("/:userId", getUser);
 
 module.exports = router;

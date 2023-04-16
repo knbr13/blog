1c7090262f050a81b7c5d2fbf590deec309ce996
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-16 22:07:50 +0300

    implemented signup function

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const User = require("../models/userModel");
+const validator = require("validator");
+
+const signup = async (req, res) => {
+    const {firstName, lastName, email, password} = req.body;
+    if(!firstName || !lastName || !email || !password) return res.status(400).json({error: "Missing some required data"});
+    if(!validator.isEmail(email)) return res.status(400).json({error: "Invalid email"});
+    if(!validator.isStrongPassword(password)) return res.status(400).json({error: "Not a strong password"});
+
+    try {
+        const userExists = await User.find({email});
+        if(userExists) return res.status(400).json({error: "Email already exists"});
+        const user = await User.create({firstName, lastName, email, password});
+        res.status(201).json({user});
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}

6407934305642edd6453e32875c9793dfbbe6130
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-16 21:54:37 +0300

    implemented generateJWTToken function

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
     firstName: {
@@ -12,7 +13,13 @@ const userSchema = mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
     },
     profilePicture: String,
     about: {
@@ -20,5 +27,27 @@ const userSchema = mongoose.Schema({
         default: "Available"
     }
 }, {timestamps: true});
+
+userSchema.pre('save', async next => {
+    if(!this.modified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+userSchema.methods.generateJWTToken = function () {
+    const user = this;
+    const token = jwt.sign(
+      { _id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '1d' }
+    );
+    return token;
+};
 
 module.exports = mongoose.model("User", userSchema);

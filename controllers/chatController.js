56de701ec3b67943a5f7cd33877e45840c9c088a
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-19 01:00:41 +0300

    check if the members contains duplicate elements

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -3,16 +3,17 @@ const Chat = require('../models/chatModel');
 
 const createChat = async (req, res) => {
     const {members, isGroup} = req.body;
-    if(!members || members.length < 1) return res.status(400).json({error: "Missing some required data"});
+    const uniqueMembers = new Set(members);
+    if(!uniqueMembers || uniqueMembers.length < 1) return res.status(400).json({error: "Missing some required data"});
     try {
-        if(members.some(element => mongoose.Types.ObjectId.isValid(element) === false));
-        members.push(req.user._id);
+        if(uniqueMembers.some(element => mongoose.Types.ObjectId.isValid(element) === false));
+        if(!uniqueMembers.includes(req.user._id)) uniqueMembers.push(req.user._id);
         let chat;
         if(isGroup){
-            chat = await Chat.create({members, isGroup, groupAdmin: req.user._id});
+            chat = await Chat.create({uniqueMembers, isGroup, groupAdmin: req.user._id});
             return res.status(201).json(chat);
         }
-        chat = await Chat.create({members});
+        chat = await Chat.create({uniqueMembers});
         res.status(201).json(chat);
     } catch (error) {
         return res.status(500).json({ message: "Internal Server Error" });

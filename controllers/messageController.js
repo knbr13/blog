bd3a705f58808da7e6ffb33d52e6f13707589dac
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-19 23:53:08 +0300

    check if the ids are valid before creating new message with senderId and chatId properties

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,15 @@
+const { default: mongoose } = require("mongoose");
 const Message = require("../models/messageModel");
 
 const createMessage = async (req, res) => {
   const { senderId, chatId, messageText } = req.body;
   if (!senderId || !chatId || !messageText)
     return res.status(400).json({ error: "Missing some required data" });
+  if (
+    !mongoose.Types.ObjectId.isValid(senderId) ||
+    !mongoose.Types.ObjectId.isValid(chatId)
+  )
+    return res.status(400).json({ error: "Invalid ID" });
   try {
     const message = await Message.create({ senderId, chatId, messageText });
     res.status(201).json(message);

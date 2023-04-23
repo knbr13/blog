7e72765be2502cf297f8c2afe0d98169a4699e83
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-23 08:12:41 +0300

    modify error handlers, throw errors instead of returning response to aviod sending two responses which will lead to 'Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client'

--- utils/validateGroupMembers.js
+++ utils/validateGroupMembers.js
@@ -7,18 +7,14 @@ const moreThanTwoMembers = (req, res, members) => {
     !uniqueMembers.size ||
     (uniqueMembers.has(req.user._id) && uniqueMembers.size === 1)
   )
-    return res
-      .status(400)
-      .json({ error: "The group should contains at least two members" });
+    throw new Error("The group should contains at least two members")
   const validSet = new Set(
     [...uniqueMembers].filter((element) =>
       mongoose.Types.ObjectId.isValid(element)
     )
   );
   if (validSet.size !== uniqueMembers.size)
-    return res
-      .status(400)
-      .json({ error: "The group should contains at least two members" });
+    throw new Error("The group should contains at least two members");
   if (!uniqueMembers.has(req.user._id)) uniqueMembers.add(req.user._id);
   return uniqueMembers;
 };

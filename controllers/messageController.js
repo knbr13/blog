a3fd4e33c89b6ed73f5ba9162dc9e1d359d40097
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-27 13:55:04 +0300

    modify getMessages function to fetch only undeleted messages

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -10,13 +10,13 @@ const createMessage = async (req, res) => {
     return res.status(400).json({ error: "Invalid ID" });
   try {
     const chat = await Chat.findById(chatId);
-    if(!chat) return res.status(404).json({error: "no such chat"});
+    if (!chat) return res.status(404).json({ error: "no such chat" });
     const message = await Message.create({
       senderId: req.user._id,
       chatId,
       messageText,
     });
-    await Chat.findByIdAndUpdate(chatId, {lastMessage: Date.now()});
+    await Chat.findByIdAndUpdate(chatId, { lastMessage: Date.now() });
     res.status(201).json(message);
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error" });
@@ -28,7 +28,12 @@ const getMessages = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(chatId))
     return res.status(400).json({ error: "Invalid Id" });
   try {
-    const messages = await Message.find({ chatId });
+    const chat = await Chat.findById(chatId);
+    const deletedAt = chat.messagesDeletedAt.filter(elem => elem.userId == req.user._id)[0].date;
+    const messages = await Message.find({
+      chatId,
+      createdAt: { $gte: deletedAt },
+    });
     res.status(200).json(messages);
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error" });

656876778f77d38646b22a5d595de5a4f25c8abf
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-27 12:33:36 +0300

    add 'messagesDeletedAt' property to Chat model

--- models/chatModel.js
+++ models/chatModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const chatSchema = mongoose.Schema({
+const chatSchema = mongoose.Schema(
+  {
     members: [
       { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     ],
@@ -8,7 +9,23 @@ const chatSchema = mongoose.Schema({
     isGroup: { type: Boolean },
     lastMessage: { type: Date, default: Date.now() },
     name: String,
-    groupPicture: String
+    groupPicture: String,
+    messagesDeletedAt: {
+      type: [
+        {
+          userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
+          },
+          date: {
+            type: Date,
+            default: Date.now(),
+          },
+        },
+      ],
+      required: true,
+    },
   },
   { timestamps: true }
 );

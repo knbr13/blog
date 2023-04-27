e55b99a7574382ce4fd1b26e8e499c9188d76e79
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-27 18:43:57 +0300

    add 'chat.isGroup' boolean to the conditions for fetching chats

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -77,7 +77,6 @@ const getChats = async (req, res) => {
       });
     const updatedChats = [];
     for (const chat of chats) {
-      console.log(chat.lastMessage.getTime(), chat.createdAt.getTime())
       if (chat.lastMessage.getTime() < chat.createdAt.getTime() || chat.isGroup) {
         updatedChats.push(chat);
       } else {

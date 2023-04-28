48c5f5bb9662e0ce75604d1628c838477a27dfc7
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-28 08:59:34 +0300

    add 'online' and 'newRoom' event listeners

--- socket.js
+++ socket.js
@@ -8,14 +8,21 @@ const initSocket = (server) => {
   });
 
   io.on("connection", (socket) => {
+    socket.on("online", ({ userId }) => {
+      socket.join(userId);
+    });
+
+    socket.on("newRoom", ({ members }) => {
+      members.forEach((user) => io.to(user).emit("fetchNewRooms"));
+    });
+
     socket.on("joinRoom", ({ chatId }) => {
       socket.join(chatId);
     });
 
     socket.on("publishMessage", ({ chatId, name }) => {
-      console.log(name);
       io.to(chatId).emit("receiveMessage", { chatId });
-      socket.to(chatId).emit("notification", name);
+      socket.to(chatId).emit("notification", { name, chatId });
     });
   });
 };

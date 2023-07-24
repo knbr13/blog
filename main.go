8cdc66a33c95b09d29d3836d39f99262448d6f2a
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-24 23:25:13 +0300

    try to append data to 3d array

--- main.go
+++ main.go
@@ -5,15 +5,21 @@ import (
 	"time"
 )
 
+var visualization = [][][]int{}
+
 func main() {
 	arr := []int{5, 2, 8, 3, 9, 8, 1, 3, 7, 4}
+	visualization = append(visualization, append([][]int{}, arr))
 	quickSort(&arr, time.Microsecond)
 	for _, v := range arr {
 		fmt.Println(v)
 	}
+	fmt.Println(visualization)
 }
 
 func quickSort(arr *[]int, delay time.Duration) {
+	m := 1
+	n := 1
 	if len(*arr) <= 1 {
 		return
 	}
@@ -28,6 +34,10 @@ func quickSort(arr *[]int, delay time.Duration) {
 	(*arr)[i+1], (*arr)[len((*arr))-1] = (*arr)[len((*arr))-1], (*arr)[i+1]
 	x := (*arr)[:i+1]
 	y := (*arr)[i+2:]
+	visualization[m] = append(visualization[m], x, append([]int{}, pivot))
+	m++
 	quickSort(&x, delay)
+	visualization[m] = append(visualization[m], y, append([]int{}, pivot))
+	n++
 	quickSort(&y, delay)
 }

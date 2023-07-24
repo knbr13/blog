d2f3f2f93052150d49542844d336180c955fdf8d
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-24 22:11:01 +0300

    use fmt.Println instead of println

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	arr := []int{5, 2, 8, 3, 9, 8, 1, 3, 7, 4}
 	quickSort(&arr, time.Microsecond)
 	for _, v := range arr {
-		println(v)
+		fmt.Println(v)
 	}
 }
 

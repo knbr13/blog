86ef2ea67f60d50f6584183a1bb0da373599a2a3
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-24 22:10:08 +0300

    fix bug, pass by reference

--- main.go
+++ main.go
@@ -4,25 +4,27 @@ import "time"
 
 func main() {
 	arr := []int{5, 2, 8, 3, 9, 8, 1, 3, 7, 4}
-	quickSort(arr, time.Microsecond)
+	quickSort(&arr, time.Microsecond)
 	for _, v := range arr {
 		println(v)
 	}
 }
 
-func quickSort(arr []int, delay time.Duration) {
-	if len(arr) <= 1 {
+func quickSort(arr *[]int, delay time.Duration) {
+	if len(*arr) <= 1 {
 		return
 	}
-	pivot := arr[len(arr)-1]
+	pivot := (*arr)[len((*arr))-1]
 	i, j := -1, 0
-	for ; j < len(arr); j++ {
-		if arr[j] < pivot {
+	for ; j < len((*arr)); j++ {
+		if (*arr)[j] < pivot {
 			i++
-			arr[i], arr[j] = arr[j], arr[i]
+			(*arr)[i], (*arr)[j] = (*arr)[j], (*arr)[i]
 		}
 	}
-	arr[i+1], arr[len(arr)-1] = arr[len(arr)-1], arr[i+1]
-	go quickSort(arr[:i+1], delay)
-	go quickSort(arr[i+2:], delay)
+	(*arr)[i+1], (*arr)[len((*arr))-1] = (*arr)[len((*arr))-1], (*arr)[i+1]
+	x := (*arr)[:i+1]
+	y := (*arr)[i+2:]
+	quickSort(&x, delay)
+	quickSort(&y, delay)
 }

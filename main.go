4de3b084c275b9ea6f1cbeb4c8190d10a28ec75c
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-31 21:02:58 +0300

    use go templates to make the page dynamic

--- main.go
+++ main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"go-arg/database"
+	"html/template"
+	"net/http"
 )
 
+type Todo struct {
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+type TodoPageData struct {
+	PageTitle string `json:"pageTitle"`
+	Todos     []Todo `json:"todos"`
+}
+
 func main() {
-	_, err := database.Connect()
-	if err != nil {
-		fmt.Println("Error connecting to database ", err)
-		return
-	}
-	// r := mux.NewRouter()
-
-	// r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-	// 	vars := mux.Vars(r)
-	// 	title := vars["title"]
-	// 	page := vars["page"]
-
-	// 	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
-	// })
-
-	// http.ListenAndServe(":8080", r)
+	templ := template.Must(template.ParseFiles("static/index.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		templ.Execute(w, data)
+	})
+	http.ListenAndServe(":8080", nil)
 }

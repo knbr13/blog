50ece4a760cbe981a510caf4939cf158d57b018e
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-31 11:40:29 +0300

    use gorilla mux to extract dynamic data from the URL

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/public/", http.StripPrefix("/public/", fs))
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	http.ListenAndServe(":8080", r)
 }

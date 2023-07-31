389dbea39b196cd16912f241e2483f72ebf8fcd2
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-31 12:17:38 +0300

    execute the code in main.go

--- main.go
+++ main.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"go-arg/database"
 )
 
 func main() {
-	r := mux.NewRouter()
+	_, err := database.Connect()
+	if err != nil {
+		fmt.Println("Error connecting to database ", err)
+		return
+	}
+	// r := mux.NewRouter()
 
-	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
+	// r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+	// 	vars := mux.Vars(r)
+	// 	title := vars["title"]
+	// 	page := vars["page"]
 
-		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
-	})
+	// 	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	// })
 
-	http.ListenAndServe(":8080", r)
+	// http.ListenAndServe(":8080", r)
 }

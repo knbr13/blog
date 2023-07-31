eb6ab25370c6eea852c3de2ae5e748bf6e5d821b
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-31 10:19:32 +0300

    add gorilla mux

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/gookit/color v1.5.4 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/sys v0.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,25 +1,2 @@
 package main
 
-func main() {
-	arr := []int{5, 2, 8, 3}
-	quickSort(&arr)
-}
-
-func quickSort(arr *[]int) {
-	if len(*arr) <= 1 {
-		return
-	}
-	pivot := (*arr)[len((*arr))-1]
-	i, j := -1, 0
-	for ; j < len((*arr)); j++ {
-		if (*arr)[j] < pivot {
-			i++
-			(*arr)[i], (*arr)[j] = (*arr)[j], (*arr)[i]
-		}
-	}
-	(*arr)[i+1], (*arr)[len((*arr))-1] = (*arr)[len((*arr))-1], (*arr)[i+1]
-	x := (*arr)[:i+1]
-	y := (*arr)[i+2:]
-	quickSort(&x)
-	quickSort(&y)
-}

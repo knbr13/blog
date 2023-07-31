ce85b55580220ab27613243383fa21701935dc1d
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-31 21:20:09 +0300

    get form data from the form

--- main.go
+++ main.go
@@ -15,8 +15,14 @@ type TodoPageData struct {
 	Todos     []Todo `json:"todos"`
 }
 
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
 func main() {
-	templ := template.Must(template.ParseFiles("static/index.html"))
+	tmpl := template.Must(template.ParseFiles("static/index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := TodoPageData{
 			PageTitle: "My TODO list",
@@ -26,7 +32,26 @@ func main() {
 				{Title: "Task 3", Done: true},
 			},
 		}
-		templ.Execute(w, data)
+		tmpl.Execute(w, data)
+	})
+	tmpl = template.Must(template.ParseFiles("forms.html"))
+
+	http.HandleFunc("/form", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		details := ContactDetails{
+			Email:   r.FormValue("email"),
+			Subject: r.FormValue("subject"),
+			Message: r.FormValue("message"),
+		}
+
+		tmpl.Execute(w, struct {
+			Success bool
+			ContactDetails
+		}{true, ContactDetails{Email: details.Email, Subject: details.Email, Message: details.Subject}})
 	})
 	http.ListenAndServe(":8080", nil)
 }

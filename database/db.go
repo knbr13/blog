ed73b06bdf429bd1950df0d4348838e7d2de59b8
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-31 12:01:11 +0300

    create table, insert into it, and get the first result

--- database/db.go
+++ database/db.go
@@ -2,6 +2,9 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"time"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -16,5 +19,49 @@ func Connect() (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	query := `
+    CREATE TABLE users (
+        id INT AUTO_INCREMENT,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        created_at DATETIME,
+        PRIMARY KEY (id)
+    );`
+
+	_, err = db.Exec(query)
+	if err != nil {
+		return nil, err
+	}
+
+	username := "johndoe"
+	password := "secret"
+	createdAt := time.Now()
+
+	result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+	if err != nil {
+		return nil, err
+	}
+	userId, err := result.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		queried_id        int
+		queried_username  string
+		queried_password  string
+		queried_createdAt time.Time
+	)
+
+	query = `SELECT id, username, password, created_at FROM users WHERE id = ?`
+	err = db.QueryRow(query, 1).Scan(&queried_id, &queried_username, &queried_password, &queried_createdAt)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("userId: ", userId)
+	fmt.Println("userName: ", queried_username)
+	fmt.Println("password: ", queried_password)
+	fmt.Println("createdAt: ", queried_createdAt)
 	return db, nil
 }

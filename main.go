bd31b5a9b84a0aa2172df971defc9d3364063652
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-01 03:42:52 +0300

    implement logging middleware

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -9,3 +11,15 @@ func main() {
 }
 
 type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+func logging() Middleware {
+	return func(hf http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			defer func() {
+				log.Println(r.URL.Path, time.Since(start))
+			}()
+			hf(w, r)
+		}
+	}
+}

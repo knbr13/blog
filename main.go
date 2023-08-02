176e4660f2b91127163515cd260d64952287d2e8
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-02 21:19:51 +0300

    use sessions to store cookies

--- main.go
+++ main.go
@@ -1,10 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/sessions"
+)
+
+var (
+	key   = []byte("secret-key")
+	store = sessions.NewCookieStore(key)
 )
 
 func main() {
-	http.HandleFunc("/", chain(sayHello, logging, method("GET")))
+	http.HandleFunc("/secret/", secret)
+	http.HandleFunc("/login/", login)
+	http.HandleFunc("/logout/", logout)
+
 	http.ListenAndServe(":8080", nil)
+}
+
+func secret(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+	fmt.Fprintln(w, "The cake is a lie!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	// Authentication goes here
+
+	session.Values["authenticated"] = true
+	session.Save(r, w)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	// Revoke users authentication
+	session.Values["authenticated"] = false
+	session.Save(r, w)
 }

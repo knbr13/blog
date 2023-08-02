29b2b465ebd4d30be80a3770b875ef1f2c16facd
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-02 21:05:51 +0300

    move middleware functions to middleware.go

--- main.go
+++ main.go
@@ -1,46 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
 	http.HandleFunc("/", chain(sayHello, logging, method("GET")))
 	http.ListenAndServe(":8080", nil)
-}
-
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
-}
-
-type Middleware func(http.HandlerFunc) http.HandlerFunc
-
-func logging(f http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		defer func() { log.Println(time.Since(start)) }()
-		f(w, r)
-	}
-}
-
-func method(name string) Middleware {
-	return func(f http.HandlerFunc) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
-			if r.Method != name {
-				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-				return
-			}
-			f(w, r)
-		}
-	}
-}
-
-func chain(f http.HandlerFunc, mds ...Middleware) http.HandlerFunc {
-	for _, md := range mds {
-		f = md(f)
-	}
-	return f
 }

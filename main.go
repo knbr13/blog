d4e3862cb28917abac7081dbe35ed4ab6ac5787b
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-02 21:33:49 +0300

    create user struct

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import (
 	"net/http"
 )
 
+type User struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Age       int    `json:"age"`
+}
+
 func main() {
 	http.ListenAndServe(":8080", nil)
 }

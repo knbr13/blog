a1a9830ce273336585b27c4562722ee2cac321d3
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-03 23:14:07 +0300

    implement hashiing and comparing password functions

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	password := "secret"
+	hash, _ := HashPassword(password)
+
+	fmt.Println("Password:", password)
+	fmt.Println("Hash:    ", hash)
+
+	match := CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }

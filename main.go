5337108579da3fbc6b4426d67a1df8a6276587d5
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-04 17:44:19 +0300

    restrict the route to GET method

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world\n"))
-	})
+	}).Methods(http.MethodGet)
 
 	log.Println("starting the server...")
 	log.Fatalln(http.ListenAndServe(":8080", router))

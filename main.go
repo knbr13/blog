3a2ff62495e1e1bec7ef0d76f2a4d4c9c94b1168
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-06 00:16:55 +0300

    try all updated methods in main func

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
 	s := New[int]()
 	s.Push(10)
-	// s.Push(30)
-	// s.Push(50)
-	// s.Push(70)
-	// s.Push(90)
+	s.Push(30)
+	s.Push(50)
+	s.Push(70)
+	s.Push(90)
 	s.Pop()
-	// fmt.Println()
+	fmt.Println()
 	fmt.Println(s)
 }

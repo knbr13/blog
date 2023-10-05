5f18674c6a054886c882fc5d32f95a7c0f4d8b46
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-05 23:49:25 +0300

    try all methods

--- main.go
+++ main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+	s := New[int]()
+	s.Push(10)
+	// s.Push(30)
+	// s.Push(50)
+	// s.Push(70)
+	// s.Push(90)
+	s.Pop()
+	// fmt.Println()
+	fmt.Println(s)
 }

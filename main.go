8d4c9d30ae8e5d009cd7dcef655f7828b6df12ea
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-05 18:55:31 +0300

    update

--- main.go
+++ main.go
@@ -2,32 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"strconv"
-
-	"github.com/gorilla/mux"
+	"time"
 )
 
 func main() {
-	router := mux.NewRouter()
-
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world\n"))
-	}).Methods(http.MethodGet)
-
-	router.HandleFunc("/post/{age}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		age := vars["age"]
-		ageInt, err := strconv.Atoi(age)
-		if err != nil || ageInt < 1 {
-			http.Error(w, "Error parsing age, please make sure it is a valid number and greater than 0", http.StatusBadRequest)
-			return
-		}
-
-		fmt.Fprintf(w, "your age is %d\n", ageInt)
-	})
-
-	log.Println("starting the server...")
-	log.Fatalln(http.ListenAndServe(":8080", router))
+	now := time.Now()
+	fmt.Println(now)
 }

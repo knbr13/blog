d12de4e22bab53f6e0260546b8c710df977c2e3b
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-05 23:15:24 +0300

    add Pop method

--- stack.go
+++ stack.go
@@ -11,3 +11,12 @@ func (s *stack[T]) Add(v T) {
 }
 
 func (s *stack[T]) Len() int { return len(*s) }
+
+func (s *stack[T]) Pop() *T {
+	if s.Len() == 0 {
+		return nil
+	}
+	deleted := (*s)[s.Len()-1]
+	*s = (*s)[:s.Len()-1]
+	return &deleted
+}

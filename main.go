6c20da31c62b038dcbf7b06af0fdff8b85caf6e1
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-12 23:19:46 +0300

    reset

--- main.go
+++ main.go
@@ -3,13 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	s := New[int]()
-	s.Push(10)
-	s.Push(30)
-	s.Push(50)
-	s.Push(70)
-	s.Push(90)
-	s.Pop()
-	fmt.Println()
-	fmt.Println(s)
+	fmt.Println("hello world")
 }

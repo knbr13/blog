f06c302783d11389d86dfc6e26a923c7277ac053
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-11-22 17:22:17 +0200

    create stringsEqual function

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool stringsEqual(const char *s1, const char *s2)
+{
+    int s1_size = sizeof(s1) / sizeof(char);
+    int s2_size = sizeof(s2) / sizeof(char);
+    if (s1_size != s2_size)
+    {
+        return false;
+    }
+
+    for (int i = 0; i < s1_size; i++)
+    {
+        if (s1[i] != s2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main()
 {
-    printf("hello world\n");
+    char s1[] = "hello world";
+    char s2[] = "hello world";
+    printf("equal?: %d\n", stringsEqual(s1, s2));
+    return 0;
 }

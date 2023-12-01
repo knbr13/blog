8e4f5aac81f3758541f2d37879735116e069a412
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-01 05:51:06 +0200

    write a custom HTTP request to a file

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	x := "GET / HTTP/1.1\r\nAccept:*/*\r\nContent-Type: text/plain\r\n\r\nhello world"
+	f, err := os.OpenFile("text.txt", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	fmt.Fprintln(f, x)
 }

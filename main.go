49df3689a35c4d820e58821e1b7c28c6673d526e
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-08 18:55:50 +0200

    reset

--- main.go
+++ main.go
@@ -1,18 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-)
-
 func main() {
-	x := "GET / HTTP/1.1\r\nAccept:*/*\r\nContent-Type: text/plain\r\n\r\nhello world"
-	f, err := os.OpenFile("text.txt", os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	fmt.Fprintln(f, x)
 }

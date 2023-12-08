143231f84d70be470bed84b39cbe3ce6abfaf6e2
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-08 19:03:09 +0200

    goroutine

--- main.go
+++ main.go
@@ -3,9 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+		log.Panicln("caught signal", map[string]string{
+			"signal": s.String(),
+		})
+		os.Exit(0)
+	}()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world\n"))
 	})

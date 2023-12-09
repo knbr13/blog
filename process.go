751bb9b64c8d8e95c82e9717dcc5ac91e16c3154
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-09 19:52:30 +0200

    delete functions

--- process.go
+++ process.go
@@ -1,9 +1,1 @@
 package main
-
-// func processRepositories(repos []string, email string) (map[int]int, error) {
-	
-// }
-
-func fillCommits(repos []string, email string) (map[int]int, error) {
-	
-}

a12c0cbaed471b0f3644c1d1704f74e731907751
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-09 19:43:50 +0200

    create scanGitRepositories function

---
+++ scan.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+)
+
+func scanGitRepositories(root string) ([]string, error) {
+	var result []string
+	err := filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("error walking %q: %v", path, err)
+		}
+
+		if info.IsDir() && info.Name() == ".git" {
+			parent := filepath.Dir(root)
+			result = append(result, parent)
+			return filepath.SkipDir
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error walking %q: %v", root, err)
+	}
+	return result, nil
+}

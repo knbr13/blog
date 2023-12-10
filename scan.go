d586d88a48cc20833f48a7f4c1e416618d89888e
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-10 06:13:11 +0200

    add logic to ignore dependency folders

--- scan.go
+++ scan.go
@@ -4,13 +4,39 @@ import (
 	"fmt"
 	"io/fs"
 	"path/filepath"
+	"slices"
+	"strings"
 )
+
+var ignoreDirectories = []string{
+	"node_modules",
+	"venv",
+	"__pycache__",
+	".venv",
+	".pytest_cache",
+	"target",
+	"bin",
+	"build",
+	".gradle",
+	"vendor",
+	"gemfile.lock",
+	"obj",
+	"packages",
+	".build",
+	"packages",
+	".dart_tool",
+	"pubspec.lock",
+}
 
 func scanGitFolders(root, email string) ([]string, error) {
 	var gitFolders []string
 	err := filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return fmt.Errorf("error accessing %q: %v", path, err)
+		}
+
+		if slices.Contains(ignoreDirectories, strings.ToLower(info.Name())) {
+			return filepath.SkipDir
 		}
 
 		if info.IsDir() && info.Name() == ".git" {

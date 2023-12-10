0a4e9aecdd88586f65ddb852f54cc677c92f48e0
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-10 07:27:53 +0200

    add getDay func

--- print.go
+++ print.go
@@ -6,10 +6,32 @@ import (
 	"time"
 )
 
+var sixEmptySpaces = strings.Repeat(" ", 6)
+
 func buildHeader(start, end time.Time) string {
 	s := strings.Builder{}
 	for current := start; current.Before(end) || current.Equal(end); current = current.AddDate(0, 1, 0) {
 		s.WriteString(fmt.Sprintf("%-16s", current.Month().String()[:3]))
 	}
 	return s.String()
+}
+
+func getDay(i int) string {
+	switch i {
+	case 0:
+		return "Sun"
+	case 1:
+		return "Mon"
+	case 2:
+		return "Tue"
+	case 3:
+		return "Wed"
+	case 4:
+		return "Thu"
+	case 5:
+		return "Fri"
+	case 6:
+		return "Sat"
+	}
+	return strings.Repeat(" ", 3)
 }

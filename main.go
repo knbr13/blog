b6bc676b0a9bfb55ad4b251d7ca2eb40e040a594
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-13 23:48:32 +0200

    try to redirect the os.Stdout stream

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	f, err := os.Create("text.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	os.Stdout = f
+	fmt.Fprintln(os.Stdout, "hello world!")
+	fmt.Println("this is me!")
+	println()
 }

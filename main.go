1e0e145a5eda872de546e2ce24f2d46f055335d1
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-13 23:24:15 +0200

    reset

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"log"
-)
-
 func main() {
-	folders, err := scanGitFolders("/home/fiber/dev")
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	commits, err := processRepos(folders, "abdullahalaadine63@gmail.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	printTable(commits)
 }

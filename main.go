640a83cd573c28b16df0292724bf2a5f30b46ed5
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-22 02:16:56 +0200

    create db

--- main.go
+++ main.go
@@ -1,42 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	p := NewPerson(WithAge(10), WithGender("Male"), WithName("Abdullah"))
-	fmt.Printf("person name: %s | person gender: %s | person age: %d\n", p.Name, p.Gender, p.Age)
-}
-
-type Person struct {
-	Name   string
-	Age    int
-	Gender string
-}
-
-type Option func(*Person)
-
-func NewPerson(options ...Option) *Person {
-	person := &Person{}
-	for _, option := range options {
-		option(person)
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/iline")
+	if err != nil {
+		log.Fatal(err)
 	}
-	return person
-}
-
-func WithName(name string) Option {
-	return func(person *Person) {
-		person.Name = name
-	}
-}
-
-func WithAge(age int) Option {
-	return func(person *Person) {
-		person.Age = age
-	}
-}
-
-func WithGender(gender string) Option {
-	return func(person *Person) {
-		person.Gender = gender
-	}
+	defer db.Close()
 }

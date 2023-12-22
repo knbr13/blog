4df292f02fe66fb08c3385926164662dcac0c8e8
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-22 21:34:29 +0200

    create getUsers function

--- main.go
+++ main.go
@@ -39,6 +39,26 @@ type User struct {
 	Name string `json:"name"`
 }
 
+func getUsers(db *sql.DB) ([]User, error) {
+	var users []User
+	rows, err := db.Query("select * from users")
+	if err != nil {
+		return users, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user User
+		err = rows.Scan(&user.ID, &user.Name)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
 func getUserByID(db *sql.DB, userID int) (*User, error) {
 	var user User
 	err := db.QueryRow("select * from users where id = ?", userID).Scan(&user.ID, &user.Name)

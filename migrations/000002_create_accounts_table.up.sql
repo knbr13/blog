e7d471753c323b9bb199f7eae31625f9167d7ea0
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-25 00:59:46 +0200

    fix error

--- migrations/000002_create_accounts_table.up.sql
+++ migrations/000002_create_accounts_table.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE `accounts` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `user_id` INTEGER NOT NULL,
+    PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 );

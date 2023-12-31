0389b92ea135e9a02328d93d2664232022f30042
abdullah-alaadine <abdullahalaadine63@gmail.com>
2024-01-01 01:46:19 +0200

    read file and copy it to a buffer

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("done!")
+	f, err := os.Open("secret.file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var buf bytes.Buffer
+	fmt.Println("buf len:", buf.Len())
+	fmt.Println("buf cap:", buf.Cap())
+	n, err := io.Copy(&buf, f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("n:", n)
+
+	fmt.Println("buf len:", buf.Len())
+	fmt.Println("buf cap:", buf.Cap())
+	fmt.Println(buf.String())
 }

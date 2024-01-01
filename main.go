e545a127ffd172856b7bf40d1f6f7a18195df299
abdullah-alaadine <abdullahalaadine63@gmail.com>
2024-01-01 05:59:58 +0200

    print the maximum length for github description

--- main.go
+++ main.go
@@ -12,4 +12,5 @@ func main() {
 	rank := fuzzy.LevenshteinDistance(s1, s2)
 	fmt.Println(rank)
 	fmt.Println((rank/(rank+1) + 1) * 25)
+	fmt.Println(len("vides an easy way to capture http related metrics (i.e. response time, bytes written, and http status code) from your application's http.Handlers. description: HTTP routing for Go 1.7 description: A delightfully tiny but powerful HTTP router for Go web applications description: A flexible and comprehensive Kotlin library for encapsulating the resul"))
 }

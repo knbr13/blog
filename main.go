d541488306549a5003a81f74328dd7152a7d83b8
abdullah-alaadine <abdullahalaadine63@gmail.com>
2024-01-01 20:53:59 +0200

    print len of string

--- main.go
+++ main.go
@@ -13,4 +13,5 @@ func main() {
 	fmt.Println(rank)
 	fmt.Println((rank/(rank+1) + 1) * 25)
 	fmt.Println(len("vides an easy way to capture http related metrics (i.e. response time, bytes written, and http status code) from your application's http.Handlers. description: HTTP routing for Go 1.7 description: A delightfully tiny but powerful HTTP router for Go web applications description: A flexible and comprehensive Kotlin library for encapsulating the resul"))
+	fmt.Println(len("binwiederhier/ntfy  https://github.com/binwiederhier/ntfy  Send push notifications t...  15205  25"))
 }

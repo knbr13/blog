c15ba58f80cdbdde9f124195d1c4a2b52e231cda
abdullah-alaadine <abdullahalaadine63@gmail.com>
2024-01-02 22:10:33 +0200

    create init function

--- logger/logger.go
+++ logger/logger.go
@@ -5,3 +5,13 @@ import "go.uber.org/zap"
 type internalLogger struct {
 	logger *zap.Logger
 }
+
+var logger internalLogger
+
+func init() {
+	zapLogger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	logger.logger = zapLogger
+}

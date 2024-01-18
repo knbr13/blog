0243fa65911c26481fb3983a74d4ce15d5250d29
abdullah-alaadine <abdullahalaadine63@gmail.com>
2024-01-18 20:30:26 +0200

    get file stats

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	err := os.WriteFile("hello.txt", []byte("hello world from hello.txt!"), 0655)
+	f, err := os.Open("hello.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
+
+	stat, err := f.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("size of file %q: %d\n", f.Name(), stat.Size())
 }

078d87ed9a0f58b6a2318c1545782771b282a17b
hrtsegv <abdullahalaadine63@gmail.com>
2026-08-13 21:09:13 +0300

    remove HTTP server implementation and log management code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,3 @@
 package main
 
-import (
-	"log"
-
-	"github.com/hrtsegv/proglog/internal/server"
-)
-
-func main() {
-	srv := server.NewHTTPServer(":8080")
-	log.Fatal(srv.ListenAndServe())
-}
+func main() {}
